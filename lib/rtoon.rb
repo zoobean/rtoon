8c50dfdd747b17c0ac02da41482c17e8322fc4be
Antonio Chavez <cavjzz@gmail.com>
2025-11-09 20:32:01 -0600

    fix: Return the parse method

--- lib/rtoon.rb
+++ lib/rtoon.rb
@@ -4,9 +4,13 @@ require_relative 'rtoon/encoder'
 module Rtoon
   VERSION = "0.1.0"
 
-  def self.decode(string)
+  def self.parse(string)
     parser = Parser.new
     parser.parse(string)
+  end
+
+  def self.decode(string)
+    parse(string)
   end
 
   def self.encode(hash, indent_level = 0)
